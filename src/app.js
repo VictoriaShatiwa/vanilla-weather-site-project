75a6f5489c080891f5f68109367cfa7759bd0346
VictoriaShatiwa <victoriashatiwa97@gmail.com>
2024-06-09 21:04:11 +0200

    updated api key

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ function updateWeather(response) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "702322ofbabded0407d2fbfca4t4f2b4";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(updateWeather);
 }
